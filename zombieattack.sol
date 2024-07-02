a30b70b64b618a9e381478fe707dbd5d034b056d
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-07-02 12:37:11 -0400

    attack comments

--- zombieattack.sol
+++ zombieattack.sol
@@ -3,28 +3,56 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
+
+    // A nonce to ensure randomness in the generated number
     uint randNonce = 0;
+
+    // Probability of winning an attack
     uint attackVictoryProbability = 70;
 
+    // Internal function to generate a random number based on a modulus
     function randMod(uint _modulus) internal returns(uint) {
+
+        // Increments the nonce to ensure different results for subsequent calls
         randNonce++;
+
+        // Generates a random number using keccak256 hash of current time, sender address, and nonce
         return uint(kecchak256(abi.encodePacked(now,msg.sender,randNonce))) % _modulus;
     }
 
+    // External function to initiate an attack from one zombie to another
     function attack(uint _zombieId, uint _targetId) external onlyOwnerOf(_zombieId) {
+
+        // Retrieves the zombies from storage
         Zombie storage myZombie = zombies[_zombieId];
         Zombie storage enemyZombie = zombies[_targetId];
 
+        // Checks if the zombie is ready to attack
         uint rand = randMod(100);
 
+        // Checks if the random number is less than or equal to the attack victory probability
         if (rand <= attackVictoryProbability) {
+
+            // Increments the win count of the attacking zombie
             myZombie.winCount++;
+
+            // Increments the level of the attacking zombie
             myZombie.level++;
+
+            // Increments the loss count of the defending zombie
             enemyZombie.lossCount++;
+
+            // Triggers the cooldown for the attacking zombie
             feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
         } else {
+
+            // Increments the loss count of the attacking zombie
             myZombie.lossCount++;
+
+            // Increments the win count of the defending zombie
             enemyZombie.winCount++;
+
+            // Triggers the cooldown for the attacking zombie
             _triggerCooldown(myZombie);
         }
     }
