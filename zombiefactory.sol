bb0044e9f759f3e4e7785587bba3ae72e6b48a40
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-07-02 12:50:06 -0400

    factory & owner comments

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -4,11 +4,16 @@ import "./ownable.sol";
 
 contract ZombieFactory is Ownable {
 
+    // Event declaration for creating a new zombie
     event NewZombie(uint zombieId, string name, uint dna);
 
+    // Variable to store the number of DNA digits
     uint dnaDigits = 16;
+
+    // Variable to calculate the modulus for DNA to ensure it's within a specific range
     uint dnaModulus = 10 ** dnaDigits;
 
+    // Struct definition for a Zombie
     struct Zombie {
         string name;
         uint dna;
@@ -18,27 +23,54 @@ contract ZombieFactory is Ownable {
         uint16 lossCount;
     }
 
+    // Array to store all zombies
     Zombie[] public zombies;
 
+    // Mapping to store the owner of a zombie
     mapping (uint => address) public zombieToOwner;
+
+    // Mapping to store the number of zombies owned by an address
     mapping (address => uint) ownerZombieCount;
 
+    // Internal function to create a zombie and add it to the zombies array
     function _createZombie(string memory _name, uint _dna) internal {
+
+        // Adds the new zombie to the zombies array
         uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTimer), 0, 0)) - 1;
+
+        // Maps the zombie ID to the owner's address
         zombieToOwner[id] = msg.sender;
+
+        // Increments the owner's zombie count
         ownerZombieCount[msg.sender]++;
+
+        // Emits a NewZombie event
         emit NewZombie(id, _name, _dna);
     }
 
+    // Internal function to generate a random DNA based on a string input
     function _generateRandomDna(string memory _str) private view returns (uint) {
+
+        // Generates a random number using keccak256 hash of the input string
         uint rand = uint(keccak256(abi.encodePacked(_str)));
+
+        // Ensures the DNA is within the range defined by dnaModulus
         return rand % dnaModulus;
     }
 
+    // Public function to create a zombie with a random DNA
     function createRandomZombie(string memory _name) public {
+
+        // Requires the owner to have no zombies
         require(ownerZombieCount[msg.sender] == 0);
+
+        // Generates a random DNA based on the provided name
         uint randDna = _generateRandomDna(_name);
+
+        // Ensures the DNA is within the valid range
         randDna = randDna - randDna % 100;
+
+        // Calls the internal function to create a new zombie
         _createZombie(_name, randDna);
     }
 
--- zombieownership.sol
+++ zombieownership.sol
@@ -1,35 +1,61 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombieattack.sol";
+
+// Imports the ERC721 contract interface for NFT functionality
 import "./erc721.sol";
 
 // Declare ERC721 inheritance here
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
+    // Maps the zombie ID to the approved address
     mapping (uint => address) zombieApprovals;
 
+    // Function to return the total number of zombies owned by an address
     function balanceOf(address _owner) external view returns (uint256) {
+
+        // Returns the number of zombies owned by the address
         return ownerZombieCount[_owner];
     }
 
+    // Function to return the owner of a specific zombie
     function ownerOf(uint256 _tokenId) external view returns (address) {
+
+        // Returns the owner of the specified zombie
         return zombieToOwner[_tokenId];
     }
 
+    // Function to transfer a zombie from one address to another
     function _transfer(address _from, address _to, uint256 _tokenId) private {
+
+        // Increases the zombie count for the new owner and decreases for the old owner
         ownerZombieCount[_to]++;
         ownerZombieCount[_from]--;
+
+        // Transfers the zombie to the new owner
         zombieToOwner[_tokenId] = _to;
+
+        // Emits a Transfer event, a requirement of the ERC721 standard
         emit Transfer(_from, _to, _tokenId);
     }
 
+    // Function to transfer a zombie from the owner's address to another address
     function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+
+        // Requires the sender to be the owner of the zombie or the approved address
         require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+
+        // Transfers the zombie to the new owner
         _transfer(_from, _to, _tokenId);
     }
 
+    // Approves another address to transfer a specific zombie
     function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
+
+        // Sets the approved address for the specified zombie
         zombieApprovals[_tokenId] = _approved;
+
+        // Emits an Approval event, a requirement of the ERC721 standard
         emit Approval(msg.sender, _approved, _tokenId);
 
     }
